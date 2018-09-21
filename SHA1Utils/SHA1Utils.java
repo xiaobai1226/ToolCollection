40013f3bd4ae2a6465094139be73f195323f11eb
高压锅里的小白 <844042976@qq.com>
2018-09-21 09:38:27 +0800

    SHA1加密算法工具类

---
+++ SHA1Utils/SHA1Utils.java
@@ -0,0 +1,41 @@
+import java.security.MessageDigest;
+
+/**
+ * <p>Title: SHA1算法</p>
+ *
+ * @author 高压锅里的小白
+ */
+public class SHA1Utils {
+    private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5',
+            '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    /**
+     * 获取原始数据的字节形式并将其加密
+     *
+     * @param bytes 要加密的原始数据的字节形式
+     * @return 加密后的字符串形式的数据
+     */
+    private static String getFormattedText(byte[] bytes) {
+        int len = bytes.length;
+        StringBuilder buf = new StringBuilder(len * 2);
+        // 把密文转换成十六进制的字符串形式
+        for (int j = 0; j < len; j++) {
+            buf.append(HEX_DIGITS[(bytes[j] >> 4) & 0x0f]);
+            buf.append(HEX_DIGITS[bytes[j] & 0x0f]);
+        }
+        return buf.toString();
+    }
+
+    public static String encode(String str) {
+        if (str == null) {
+            return null;
+        }
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+            messageDigest.update(str.getBytes());
+            return getFormattedText(messageDigest.digest());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
